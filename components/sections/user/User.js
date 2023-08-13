e01c2451739dda5b9518d47c49cd306337a0e43b
@askcasmir <casmirconsults@gmail.com>
2023-08-13 14:25:02 -0400

    WIP: signout page with updated ipfs link/cid

--- components/sections/user/User.js
+++ components/sections/user/User.js
@@ -6,7 +6,7 @@ import React from 'react'
 
 
 const User = ({ getOnlyImage }) => {
-    const name = useSelector(state => state.user.name || 'Default Name');
+    const name = useSelector(state => state.user.name || 'Account');
     const uPic = useSelector(state => state.user.upic); // Use the correct property name
     const uName = useSelector(state => state.user.uname); // Use the correct property name
 
@@ -28,7 +28,7 @@ const User = ({ getOnlyImage }) => {
                 <img className={styles.avatar} src={uPic}/>
                 <div className={styles.avatarTextContainer}>
                     <p className={styles.name}>{name}</p>
-                    <p className={styles.username}>@{uName}</p>
+                    <p className={styles.username}>0x{uName}</p>
                 </div>
                 <DropdownButton id='dropDownUser' title='' className={styles.dropdownToggle}>
                         <Button isDropdownButton={1} text='Messages' type='secondary'/>
